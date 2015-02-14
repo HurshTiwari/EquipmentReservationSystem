4d51dfb035f05a76b3877649677fb1261b3fb704
HurshTiwari <harshdude95@gmail.com>
2015-02-14 20:20:17 +0530

    First code files

---
+++ common/navbar_top_side.php
@@ -0,0 +1,67 @@
+
+<body>
+    <div id="wrapper">
+
+        <!-- Navigation -->
+        <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="index.html">IIT INDORE</a>
+            </div>
+            <!-- Top Menu Items -->
+            <ul class="nav navbar-right top-nav">
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user"></i>  User Name <b class="caret"></b></a>
+                    <ul class="dropdown-menu">
+                        <li>
+                            <a href="#"><i class="fa fa-fw fa-user"></i> Profile</a>
+                        </li>
+                        <li>
+                            <a href="#"><i class="fa fa-fw fa-gear"></i> Settings</a>
+                        </li>
+                        <li class="divider"></li>
+                        <li>
+                            <a href="login.html"><i class="fa fa-fw fa-power-off"></i> Log Out</a>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+            <!-- Sidebar Menu Items - These collapse to the responsive navigation menu on small screens -->
+            <div class="collapse navbar-collapse navbar-ex1-collapse">
+                <ul class="nav navbar-nav side-nav">
+                    <!--<li class="sidebar-search">
+                    <div class="input-group custom-search-form">
+                        <input type="text" class="form-control" placeholder="Search..">
+                        <span class="input-group btn">
+                         <button class="btn btn-inverse" type="button">
+                          <i class="fa fa-search">
+                          </i>
+                        </button>
+                        </span>
+                    </div>
+                    </li>-->
+                    <li class="active">
+                        <a href="index.html"><i class="fa fa-fw fa-tasks"></i> Equipments</a>
+                    </li>
+                    <li>
+                        <a href="charts.html"><i class="fa fa-fw fa-users"></i> All Users</a>
+                    </li>
+                    <li>
+                        <a href="tables.html"><i class="fa fa-fw fa-user"></i> Add user</a>
+                    </li>
+                    <li>
+                        <a href="forms.html"><i class="fa fa-fw fa-plus"></i> Add equipment</a>
+                    </li>
+                    <li>
+                        <a href="bootstrap-elements.html"><i class="fa fa-fw fa-remove"></i> Cancel Booking</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </nav>
---
+++ index.php
@@ -0,0 +1,103 @@
+<!DOCTYPE php>
+<?php include_once("/common/head.php");?>
+<?php include_once("/common/navbar_top_side.php");?>
+
+        <div id="page-wrapper">
+            <div class="container-fluid">
+
+                <!-- Page Heading -->
+                <div class="row">
+                    <div class="col-lg-12">
+                        <h1 class="page-header">
+                            <center>Equipments</center>
+                        </h1>
+                        <ul class="alert alert-success alert-dismissable">
+                            <li>
+                                <i class="fa fa-tag"></i> Click at the links below to get the ful description of the equipment 
+								<button type="button" class="close" data-dismiss="alert" aria-hidden="true">
+								&times;
+								</button>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <!-- /.row -->
+                <div class="row">
+                <div class="col-sm-6 col-md-4">
+                    <a href="#">
+                        <img class="img-responsive" src="http://placehold.it/700x400" alt>
+                    </a>
+                    <a href="#">
+                    <h3>EquipmentName1</h3>
+                    </a>
+                    <p>
+                      Description..............................................................
+                    </p>
+                </div>
+                <div class="col-sm-6 col-md-4">
+                    <a href="#">
+                        <img class="img-responsive" src="http://placehold.it/700x400" alt>
+                    </a>
+                    <a href="#">
+                    <h3>EquipmentName2</h3>
+                    </a>
+                    <p>
+                      Description..............................................................
+                    </p>
+                </div>
+                 <div class="col-sm-6 col-md-4">
+                    <a href="#">
+                        <img class="img-responsive" src="http://placehold.it/700x400" alt>
+                    </a>
+                    <a href="#">
+                    <h3>EquipmentName2</h3>
+                    </a>
+                    <p>
+                      Description..............................................................
+                    </p>
+                </div>
+                </div>
+                <!--/. row-->
+                <div class="row">
+                <div class="col-sm-6 col-md-4">
+                    <a href="#">
+                        <img class="img-responsive" src="http://placehold.it/700x400" alt>
+                    </a>
+                    <a href="#">
+                    <h3>EquipmentName1</h3>
+                    </a>
+                    <p>
+                      Description..............................................................
+                    </p>
+                </div>
+                <div class="col-sm-6 col-md-4">
+                    <a href="#">
+                        <img class="img-responsive" src="http://placehold.it/700x400" alt>
+                    </a>
+                    <a href="#">
+                    <h3>EquipmentName2</h3>
+                    </a>
+                    <p>
+                      Description..............................................................
+                    </p>
+                </div>
+                 <div class="col-sm-6 col-md-4">
+                    <a href="#">
+                        <img class="img-responsive" src="http://placehold.it/700x400" alt>
+                    </a>
+                    <a href="#">
+                    <h3>EquipmentName2</h3>
+                    </a>
+                    <p>
+                      Description..............................................................
+                    </p>
+                </div>
+                </div>
+                <!--/. row-->
+            </div>
+            <!-- /.container-fluid -->
+
+        </div>
+        <!-- /#page-wrapper -->
+
+    <?php include_once("/common/close.php")?>
